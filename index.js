77c66a2bb48fc8be2795de00606519fbcd144848
Keven Wendler <keven.nikolas123@gmail.com>
2023-06-06 16:21:57 -0300

    fix: corrige erro ao usar mmiddleware

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(compression());
-app.use(helmet);
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
