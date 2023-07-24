4cc083f65205715b2286a1914cb6c82c0fe138e4
Keven Wendler <keven.nikolas123@gmail.com>
2023-07-24 09:40:44 -0400

    fix: corrige timestamp das capturas

--- controllers/telemetria.js
+++ controllers/telemetria.js
@@ -16,8 +16,8 @@ router.post('/ocr', async (req, res) => {
     ...body,
     captura_data_hora: body.captura_data_hora !== null ? dayjs(body.captura_data_hora).toISOString() : body.captura_data_hora,
     registro_data_hora: body.registro_data_hora !== null ? dayjs(body.registro_data_hora).toISOString() : body.registro_data_hora,
-    envio_tentativa_data_hora: body.envio_tentativa_data_hora !== null ? dayjs(body.envio_tentativa_data_hora).toISOString() : body.envio_tentativa_data_hora,
-    envio_data_hora: body.envio_data_hora !== null ? dayjs(body.envio_data_hora).toISOString() : body.envio_data_hora,
+    envio_tentativa_data_hora: body.envio_tentativa_data_hora !== null ? dayjs.utc(body.envio_tentativa_data_hora).utcOffset(-4).format() : body.envio_tentativa_data_hora,
+    envio_data_hora: body.envio_data_hora !== null ? dayjs.utc(body.envio_data_hora).utcOffset(-4).format() : body.envio_data_hora,
     captura_resolucao
   }
 
